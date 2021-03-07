c45c23eede599657d3e7ae846d6e243cdace5179
MikeVasaski <71443611+MikeVasaski@users.noreply.github.com>
2021-03-07 23:46:03 +0700

    Create Simple.java

---
+++ Simple.java
@@ -0,0 +1,12 @@
+public class Simple{
+    
+    private String name;
+    
+    public static void main(String[] args) {
+        Simple myData = new Simple();
+
+        myData.name = "Jeerasak Tarat";
+
+        System.out.println("Just test : Name : "+myData.name.toString());
+    }
+}
